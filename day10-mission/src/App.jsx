6faceafd8ef169652cec708803916ef593b23610
Yoonjeong Ko <jejukyj@naver.com>
2025-01-31 21:39:40 +0900

    Initial Commit

---
+++ day10-mission/src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Welcome from "./components/Welcome";
+
+function App() {
+  return (
+    <>
+      <Welcome name={"와플"} isMember={true} />
+    </>
+  );
+}
+
+export default App;
